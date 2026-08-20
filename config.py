6858e6a2b7c2df2ae807161e56ba2c6852da70aa
Anesh2302 <simonpetercys@gmail.com>
2026-08-20 19:32:55 +0530

    Add Render.com deployment: PostgreSQL support, render.yaml, Procfile

--- config.py
+++ config.py
@@ -11,8 +11,10 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", os.getenv("SECRET_KEY", secrets.token_hex(32)))
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "mysql+pymysql://insyrium_app:InsyriumApp2026!@localhost:3306/insyrium",
+        "sqlite:///insyrium.db",
     )
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
